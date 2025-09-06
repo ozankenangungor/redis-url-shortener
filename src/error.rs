7ce4522fa2db16da91aa380be10423ad33818d26
Ozan Kenan Güngör <ozankenangungor@Ozan-MacBook-Pro.local>
2025-09-06 09:42:49 +0300

    thiserror update and adding docker-compose

--- src/error.rs
+++ src/error.rs
@@ -2,56 +2,39 @@ use axum::{
     http::StatusCode,
     response::{IntoResponse, Response},
 };
+use thiserror::Error;
 use tracing::error;
 
+#[derive(Error, Debug)]
 pub enum AppError {
-    PoolError(deadpool_redis::PoolError),
-    RedisError(redis::RedisError),
+    #[error("URL not found")]
     NotFound,
+
+    #[error("An unexpected Redis pool error occurred")]
+    PoolError(#[from] deadpool_redis::PoolError),
+
+    #[error("An unexpected Redis command error occurred")]
+    RedisError(#[from] redis::RedisError),
+
+    #[error("Data integrity error: Found invalid URL in storage: {0}")]
     InvalidUrlInStorage(String),
 }
 
 impl IntoResponse for AppError {
     fn into_response(self) -> Response {
-        let (status, error_message) = match self {
-            AppError::NotFound => (StatusCode::NOT_FOUND, "URL Not Found".to_string()),
-            AppError::PoolError(e) => {
-                error!("Redis pool error: {}", e);
+        error!("{self}");
+
+        let (status, user_message) = match self {
+            AppError::NotFound => (StatusCode::NOT_FOUND, self.to_string()),
+
+            AppError::PoolError(_) | AppError::RedisError(_) | AppError::InvalidUrlInStorage(_) => {
                 (
                     StatusCode::INTERNAL_SERVER_ERROR,
-                    "Internal server error".to_string(),
-                )
-            }
-            AppError::RedisError(e) => {
-                error!("Redis command error: {}", e);
-                (
-                    StatusCode::INTERNAL_SERVER_ERROR,
-                    "Internal server error".to_string(),
-                )
-            }
-            AppError::InvalidUrlInStorage(invalid_url) => {
-                error!(
-                    "Data integrity error: Found invalid URL in storage: {}",
-                    invalid_url
-                );
-                (
-                    StatusCode::INTERNAL_SERVER_ERROR,
-                    "Internal server error".to_string(),
+                    "An internal server error occurred".to_string(),
                 )
             }
         };
-        (status, error_message).into_response()
-    }
-}
 
-impl From<deadpool_redis::PoolError> for AppError {
-    fn from(e: deadpool_redis::PoolError) -> Self {
-        AppError::PoolError(e)
-    }
-}
-
-impl From<redis::RedisError> for AppError {
-    fn from(e: redis::RedisError) -> Self {
-        AppError::RedisError(e)
+        (status, user_message).into_response()
     }
 }
